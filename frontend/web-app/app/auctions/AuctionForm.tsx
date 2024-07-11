97b0d04c2ed8d38d6ad3c108eeba7c61b74095dd
Paulo Braz <ph_braz@hotmail.com>
2024-07-11 14:31:43 +0100

    form changes

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,16 +1,23 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import { Button, TextInput } from "flowbite-react";
+import Input from "@/app/components/input";
 
 const AuctionForm = () => {
   const {
-    register,
+    control,
     handleSubmit,
     setFocus,
     formState: { isSubmitting, isValid, isDirty, errors },
-  } = useForm();
+  } = useForm({
+    mode: "onTouched",
+  });
+
+  useEffect(() => {
+    setFocus("make");
+  }, [setFocus]);
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -18,29 +25,72 @@ const AuctionForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col mt-3">
-      <div className="mb-3 block px-2">
-        <TextInput
-          {...register("make", { required: "make is required" })}
-          placeholder="Make"
-          color={errors?.make && "failure"}
-          helperText={errors.make?.message as string}
+      <Input
+        label="Make"
+        name="make"
+        control={control}
+        rules={{ required: "Make is required" }}
+      />
+      <Input
+        label="Model"
+        name="model"
+        control={control}
+        rules={{ required: "Model is required" }}
+      />
+      <Input
+        label="Color"
+        name="color"
+        control={control}
+        rules={{ required: "Color is required" }}
+      />
+
+      <div className="grid grid-cols-2 gap-3">
+        <Input
+          label="Year"
+          name="year"
+          control={control}
+          type="number"
+          rules={{ required: "Year is required" }}
+        />
+        <Input
+          label="Mileage"
+          name="mileage"
+          control={control}
+          type="number"
+          rules={{ required: "Color is required" }}
         />
       </div>
-      <div className="mb-3 block px-2">
-        <TextInput
-          {...register("model", { required: "Model is required" })}
-          placeholder="Model"
-          color={errors?.model && "failure"}
-          helperText={errors.model?.message as string}
+
+      <Input
+        label="Image Url"
+        name="imageUrl"
+        control={control}
+        rules={{ required: "Image url is required" }}
+      />
+
+      <div className="grid grid-cols-2 gap-3">
+        <Input
+          label="Reserve price (enter 0 if no reserve)"
+          name="reservePrice"
+          control={control}
+          type="number"
+          rules={{ required: "Reserve price is required" }}
+        />
+        <Input
+          label="Auction end date/time"
+          name="auctionEnd"
+          control={control}
+          type="date"
+          rules={{ required: "Auction end date is required" }}
         />
       </div>
-      <div className="flex justify-between px-2 mb-3">
+
+      <div className="flex justify-between mb-3">
         <Button outline color="gray">
           Cancel
         </Button>
         <Button
           isProcessing={isSubmitting}
-          disabled={!isValid}
           type="submit"
           outline
           color="success"
--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -4,7 +4,7 @@ import AuctionForm from "@/app/auctions/AuctionForm";
 
 const Create = () => {
   return (
-    <div className="mx-auto max-w-[75%] shadow-lg bg-white rounded-lg">
+    <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
       <Heading
         title="Sell your car"
         subtitle="Please enter the details of your car"
