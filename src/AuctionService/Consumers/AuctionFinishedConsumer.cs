124cda1f98092cd316ac9f945b0eec10443683d1
Paulo Braz <ph_braz@hotmail.com>
2024-07-21 13:53:32 +0100

    Bidding Services.

--- src/AuctionService/Consumers/AuctionFinishedConsumer.cs
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -18,7 +18,7 @@ public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
     public async Task Consume(ConsumeContext<AuctionFinished> context)
     {
         Console.WriteLine("--> Consuming auction finished consumer");
-        var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
 
         if (context.Message.ItemSold)
         {
